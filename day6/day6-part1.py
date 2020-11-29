33190ed528bd1210fd245638500efb57f684f836
Skyrail <aled.skyrail@gmail.com>
2020-11-29 14:47:33 +0000

    Day 6 - part 1 - works for the test data...but not the actual data. Going to be interesting to debug

--- day6/day6-part1.py
+++ day6/day6-part1.py
@@ -9,8 +9,24 @@ if os.path.isfile(inputPath):
         
         orbits = orbitFile.read().split()
 
+        orbitCount, orbitMap = 0, {}
+
         for orbit in orbits:
-            print(f'Orbit: {orbit}')
+            orbiting,orbitee = orbit.split(')')
+
+            if orbiting in orbitMap:
+                orbitMap[orbiting].append(orbitee)
+            else:
+                orbitMap[orbiting] = [orbitee]
+
+            for planet in orbitMap:
+                if orbiting in orbitMap[planet]:
+                    orbitMap[planet].append(orbitee)
+
+        for _,orbitMapOrbits in orbitMap.items():
+            orbitCount += len(orbitMapOrbits)
+
+        print(f'Total orbit count is {orbitCount}')
 
     except IOError:
         print(f'Unable to open file {inputPath}')
